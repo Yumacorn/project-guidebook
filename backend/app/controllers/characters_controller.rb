703a6728e3f17129b9780434ec9a51a3ba44798a
Yumacorn <ReachRyanMui@gmail.com>
2021-01-31 12:34:37 -0500

    updated controller actions

--- backend/app/controllers/characters_controller.rb
+++ backend/app/controllers/characters_controller.rb
@@ -1,7 +1,9 @@
 class CharactersController < ApplicationController
 
+    before_action :set_universe
     def index
-
+        @characters = @universe.characters
+        render json: @characters
     end
 
     def create
@@ -9,14 +11,28 @@ class CharactersController < ApplicationController
     end
 
     def update
-
+        @universe = Universe.find(params[:id])
+        binding.pry
+        render json: @universe
     end
 
     def show
-
+        @character = Character.find(params[:id])
+        render json: @character        
     end
     
     def destroy
+        @character = Character.find(params[:id])
+        @character.destroy
+    end
 
+    private
+
+    def set_universe 
+        @universe = Universe.find(params[:universe_id])
+    end
+
+    def character_params
+        params.require(:character).permit(:name, :type_of, :universe_id, :description)
     end
 end
--- backend/app/controllers/universes_controller.rb
+++ backend/app/controllers/universes_controller.rb
@@ -1,22 +1,38 @@
 class UniversesController < ApplicationController
 
     def index
-
+        @universes = Universe.all
+        render json: @universes
     end
 
     def create
-
+        @universe = Universe.new(universe_params)
+        if @universe.save
+            render json: @universe
+        else
+            render json: {error_message: "Failed to create universe"}
+        end
     end
 
     def update
-
+        @universe = Universe.find(params[:id])
+        binding.pry
+        render json: @universe
     end
 
     def show
-
+        @universe = Universe.find(params[:id])
+        render json: @universe
     end
 
     def destroy
+        @universe = Universe.find(params[:id])
+        @universe.destroy
+    end
 
+    private
+
+    def universe_params
+        params.require(:universe).permit(:name, :type_of)
     end
 end

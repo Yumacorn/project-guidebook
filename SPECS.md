bf7237f1a80db65282c46e6a205a1d16402dac9c
Yumacorn <ReachRyanMui@gmail.com>
2021-01-31 16:14:08 -0500

    savepoint before erasing all non-created front-end react items

--- SPECS.md
+++
@@ -1,2 +0,0 @@
-Here is where ths SPECS for the project should go
-Nice
--- project-guidebook/src/index.js
+++ project-guidebook/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import universeReducer from './reducers/universeReducer'
 
-import App from './App';
+import App from './App'
 
 const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 

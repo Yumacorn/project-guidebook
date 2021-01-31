d807f0b1534dfc27ce024b161a3f9bd6cb054e84
Yumacorn <ReachRyanMui@gmail.com>
2021-01-31 15:42:05 -0500

    added store and reducer to index.js for app-wide access

--- project-guidebook/src/App.js
+++ project-guidebook/src/App.js
@@ -1,21 +1,8 @@
 import React, { Component } from 'react'
 import ReactDom from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
+
 
 export default class App extends Component {
-
-
-  componentDidMount() {
-    debugger
-    fetch('http://localhost:3000/universes')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      debugger
-    })
-    }
 
   render() {
     return (
--- project-guidebook/src/index.js
+++ project-guidebook/src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+let store = createStore(reducer, enhancer(applyMiddleware(thunk)))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+        , 
+    document.getElementById('root'))

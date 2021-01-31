584ed4142acb2dad5fd679b73d07315aa5cbc8d0
Yumacorn <ReachRyanMui@gmail.com>
2021-01-31 18:48:22 -0500

    added input form for Universe model

--- project-guidebook/src/components/UniverseInput.js
+++ project-guidebook/src/components/UniverseInput.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react'
 
 export default class UniverseInput extends Component {
+
+    state = {
+        name: '',
+        type_of: ''
+    }
+
+
     render(){
         return(
             <div>
-                UniverseInput
+                <form>
+                    <label>Enter name of new universe </label>
+                    <input type='text' placeholder='Name' value={this.state.name}></input><br/>
+                    <label>Enter type of new universe </label>
+                    <input type='text' placeholder='Type' value={this.state.type_of}></input><br/>
+                </form>
             </div>
         )
     }

b8725b3d5833314e8835e001090c852298377326
Yumacorn <ReachRyanMui@gmail.com>
2021-01-31 17:52:06 -0500

    workaround for npm start not recognizing app root, use url instead of localhost

--- project-guidebook/src/App.js
+++ project-guidebook/src/App.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {fetchUniverses} from './actions/fetchUniverses';
 
-export default class App extends Component {
+
+class App extends React.Component {
 
   componentDidMount() {
     fetch('http://localhost:3000/universes/1/characters')
@@ -16,3 +19,29 @@ export default class App extends Component {
     )
   }
 }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     universes: state.universes
+//   }
+// }
+
+// const 
+
+
+export default connect(null, {fetchUniverses})(App);
+
+// import React from 'react';
+
+// class App extends React.Component {
+//   render() {
+//     debugger
+//     return (
+//       <div className="App">
+//         App
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
--- project-guidebook/src/index.js
+++ project-guidebook/src/index.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
-import universeReducer from './reducers/universeReducer'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import universeReducer from './reducers/universeReducer';
 
-import App from './App'
+import App from './App';
 
 const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 let store = createStore(universeReducer, enhancer(applyMiddleware(thunk)))
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
-        , 
-    document.getElementById('root'))
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import App from './App';
+
+// debugger
+// ReactDOM.render(<App />, document.getElementById('root'));

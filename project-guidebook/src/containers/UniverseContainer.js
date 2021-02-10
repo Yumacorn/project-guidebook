0b8eacfd52dce0d054bda2a808474c824eec8053
Yumacorn <ReachRyanMui@gmail.com>
2021-02-09 21:29:19 -0500

    reintroducing input form and routing

--- project-guidebook/src/containers/UniverseContainer.js
+++ project-guidebook/src/containers/UniverseContainer.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import {fetchUniverses} from '../actions/fetchUniverses'
 import Universes from '../components/Universes'
-// import UniverseInput from '../components/UniverseInput'
-// import {Route} from 'react-router-dom'
+import UniverseInput from '../components/UniverseInput'
+import {Route} from 'react-router-dom'
 // import Universe from '../components/Universe'
 // import Characters from '../components/Characters'
 
@@ -19,7 +19,7 @@ class UniverseContainer extends Component {
         return (
             <div>
                 -Universe Container.js:<br/><br/>
-                {/* <Route path='/universes/new' component={UniverseInput} /> */}
+                <Route path='/universes/new' component={UniverseInput} />
                 <Universes universes={this.props.universes}/>
                 {/* <Route path='/universes/:id' render={(routerProps) => <Universe {...routerProps} universes={this.props.universes} /> } /> */}
                 {/* <Route exact path='/universes' render={(routerProps) => <Universes {...routerProps} universes={this.props.universes} /> } /> */}
